515466c2a2e55e2c2f4d8d92b128b560630ed21d
rail5 <andrew@rail5.org>
2025-09-08 18:38:25 +0800

    Move macro definition to global ext.h

--- src/ext/RemapSdlButton.h
+++ src/ext/RemapSdlButton.h
@@ -5,15 +5,7 @@
 
 #pragma once
 
-/**
- * Only declare these functions to have C external linkage **if** we're in a C++ compiler.
- * If we're in a C compiler, then obviously they already have C linkage.
- */
-#ifdef __cplusplus
-	#define EXTERN_C extern "C"
-#else
-	#define EXTERN_C
-#endif
+#include "ext.h"
 
 EXTERN_C int RemapSdlButton(int button);
 EXTERN_C void ChangeSdlButtonMapping(int sdlButton, int internalButton);
